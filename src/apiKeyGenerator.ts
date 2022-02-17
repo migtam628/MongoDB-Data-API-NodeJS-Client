18548d118d8a35b96bffdffcb8fae4f4b7ec5b36
Mig Tam Rod <migtam628@gmail.com>
2022-02-17 14:31:20 -0500

    Update apiKeyGenerator.ts
    update: changed the length of the api keys from 8 characters to 12-16 characters

--- src/apiKeyGenerator.ts
+++ src/apiKeyGenerator.ts
@@ -2,7 +2,7 @@ import * as crypto from "crypto";
 import { MongoDBAPIRequest } from "./MongoDBAPIRequest";
 
 export function apiKeyGenerator(req: any, res: any) {
-  const apiKey = crypto.randomBytes(8).toString("hex");
+  const apiKey = crypto.randomBytes(getRandomInt(12, 16)).toString("hex");
   const { collection, database, dataSource } = req.body;
   let request: TInsertOneBody = {
     action: "insertOne",
@@ -11,7 +11,7 @@ export function apiKeyGenerator(req: any, res: any) {
     dataSource: dataSource,
     document: {
       apiKey: apiKey,
-      apiKeyExpiration: new Date().getTime() + 1000 * 60 * 60 * 24 * 365,
+      apiKeyExpiration: new Date().getTime() + 1000 * 60 * 60 * 24 * 730,
     },
   };
 
@@ -25,17 +25,31 @@ export function apiKeyGenerator(req: any, res: any) {
   }
 
   if (collection.length < 1) respond400("collection");
-
   if (database.length < 1) respond400("database");
-
   if (dataSource.length < 1) respond400("dataSource");
 
   MongoDBAPIRequest(request, (r) =>
     res.json({
       status: r.name || "OK",
-      data: r,
-      apiKey: apiKey,
+      data: {
+        ...r,
+        apiKey: apiKey,
+        apiKeyExpiration: msToDays(
+          new Date().getTime() + 1000 * 60 * 60 * 24 * 730
+        ),
+      },
       statusCode: r.status || 200,
     })
   );
+}
+
+function msToDays(ms: number) {
+  const date = new Date(ms).toLocaleString();
+  return date;
+}
+
+function getRandomInt(min: number, max: number) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
 }
