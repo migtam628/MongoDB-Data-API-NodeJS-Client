7103bdd0ce149ef1509a07af2b210df2dd829ddd
Mig Tam Rod <migtam628@gmail.com>
2022-02-20 18:01:32 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,54 +2,35 @@ import * as functions from "firebase-functions";
 import * as express from "express";
 import { apiKeyValidator } from "./apiKeyValidator";
 import { Initialize } from "./Initialize";
+import { Aggregate } from "./endpoints/Aggregate";
+import { FindAll } from "./endpoints/FindAll";
+import { FindOne } from "./endpoints/FindOne";
+import { DeleteOne } from "./endpoints/DeleteOne";
+import { UpdateOne } from "./endpoints/UpdateOne";
+import { UpdateMany } from "./endpoints/UpdateMany";
+import { DeleteMany } from "./endpoints/DeleteMany";
+import { ReplaceOne } from "./endpoints/ReplaceOne";
+import { InsertOne } from "./endpoints/InsertOne";
+import { InsertMany } from "./endpoints/InsertMany";
+import { apiKeyGenerator } from "./apiKeyGenerator";
 
 const app: express.Application = express();
 Initialize(app);
 
-app.use((req, res, next) => apiKeyValidator(req, res, next));
+app.use(apiKeyValidator);
+app.post("/aggregate", Aggregate);
+app.post("/find-all", FindAll);
+app.post("/find-one", FindOne);
+app.post("/delete-many", DeleteMany);
+app.post("/delete-one", DeleteOne);
+app.post("/insert-one", InsertOne);
+app.post("/update-one", UpdateOne);
+app.post("/update-many", UpdateMany);
+app.post("/insert-many", InsertMany);
+app.post("/replace-one", ReplaceOne); 
+app.post("/api-key-generator", apiKeyGenerator);
 
-import("./endpoints/Aggregate").then((c) => {
-  app.post("/aggregate", c.Aggregate);
-});
-
-import("./endpoints/FindAll").then((c) => {
-  app.post("/find-all", c.FindAll);
-});
-
-import("./endpoints/FindOne").then((c) => {
-  app.post("/find-one", c.FindOne);
-});
-
-import("./endpoints/DeleteMany").then((c) => {
-  app.post("/delete-many", c.DeleteMany);
-});
-
-import("./endpoints/DeleteOne").then((c) => {
-  app.post("/delete-one", c.DeleteOne);
-});
-
-import("./endpoints/InsertOne").then((c) => {
-  app.post("/insert-one", c.InsertOne);
-});
-
-import("./endpoints/UpdateOne").then((c) => {
-  app.post("/update-one", c.UpdateOne);
-});
-
-import("./endpoints/UpdateMany").then((c) => {
-  app.post("/update-many", c.UpdateMany);
-});
-
-import("./endpoints/InsertMany").then((c) => {
-  app.post("/insert-many", c.InsertMany);
-});
-
-import("./endpoints/ReplaceOne").then((c) => {
-  app.post("/replace-one", c.ReplaceOne);
-});
-
-import("./apiKeyGenerator").then((c) => {
-  app.post("/api-key-generator", c.apiKeyGenerator);
-});
+console.log(process.env.API_KEY);
+console.log(process.env.URL_ENDPOINT);
 
 export const mongoDBApiServer = functions.https.onRequest(app);
